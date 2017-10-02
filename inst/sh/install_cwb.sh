b084951a5a90c04f2134840cfaf64a67a3572d97
Andreas Blätte <andreas.blaette@uni-due.de>
2017-10-03 01:41:44 +0200

    install perl module for CWB

--- inst/sh/install_cwb.sh
+++ inst/sh/install_cwb.sh
@@ -4,5 +4,12 @@ mkdir ~/tmp
 cd ~/tmp
 wget https://sourceforge.net/projects/cwb/files/cwb/cwb-3.0.0/cwb-3.0.0-linux-x86_64.tar.gz
 tar xzfv cwb-3.0.0-linux-x86_64.tar.gz
-cd cwb-3.0.0-linux-x86_64
-./install-cwb.sh
+./cwb-3.0.0-linux-x86_64/install-cwb.sh
+
+wget http://cwb.sourceforge.net/temp/Perl-CWB-2.2.102.tar.gz
+tar xzfv Perl-CWB-2.2.102.tar.gz
+cd CWB-2.2.102
+perl Makefile.PL
+make
+make test
+make install
