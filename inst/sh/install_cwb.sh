ec4fd08832d15e4a94bd73c474d2e89c7262e4c8
Andreas Blätte <andreas.blaette@uni-due.de>
2017-10-03 02:26:05 +0200

    installing CWB

--- inst/sh/install_cwb.sh
+++ inst/sh/install_cwb.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-mkdir /opt/cwb
-mkdir /opt/cwb/bin
-mkdir /opt/cwb/include
-mkdir /opt/cwb/lib
-mkdir /opt/share
-mkdir /opt/share/man
+# mkdir /opt/cwb
+# mkdir /opt/cwb/bin
+# mkdir /opt/cwb/include
+# mkdir /opt/cwb/lib
+# mkdir /opt/share
+# mkdir /opt/share/man
+# cd /opt/cwb
 
-cd /opt/cwb
+mkdir ~/tmp
+cd ~/tmp
 wget https://sourceforge.net/projects/cwb/files/cwb/cwb-3.0.0/cwb-3.0.0-linux-x86_64.tar.gz
 tar xzfv cwb-3.0.0-linux-x86_64.tar.gz
-sed -i -e "s#PREFIX='/usr/local'#PREFIX='/opt/cwb/'#" ./cwb-3.0.0-linux-x86_64/install-cwb.sh
-./cwb-3.0.0-linux-x86_64/install-cwb.sh
-export PATH=$PATH:/opt/cwb/bin
+# sed -i -e "s#PREFIX='/usr/local'#PREFIX='/opt/cwb/'#" ./cwb-3.0.0-linux-x86_64/install-cwb.sh
+cd cwb-3.0.0-linux-x86_64
+# ./cwb-3.0.0-linux-x86_64/install-cwb.sh
+./install-cwb.sh
+# export PATH=$PATH:/opt/cwb/bin
 
+cd ..
 wget http://cwb.sourceforge.net/temp/Perl-CWB-2.2.102.tar.gz
 tar xzfv Perl-CWB-2.2.102.tar.gz
 cd CWB-2.2.102
