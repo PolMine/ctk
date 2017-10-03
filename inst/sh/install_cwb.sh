3cfa4b8d622e96e1aad7077e96c8a3971f6085ee
Andreas Blätte <andreas.blaette@uni-due.de>
2017-10-03 02:10:28 +0200

    next try

--- inst/sh/install_cwb.sh
+++ inst/sh/install_cwb.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-mkdir ~/tmp
-cd ~/tmp
+mkdir /opt/cwb
+mkdir /opt/cwb/bin
+mkdir /opt/cwb/include
+mkdir /opt/cwb/lib
+mkdir /opt/share
+mkdir /opt/share/man
+
+cd /opt/cwb
 wget https://sourceforge.net/projects/cwb/files/cwb/cwb-3.0.0/cwb-3.0.0-linux-x86_64.tar.gz
 tar xzfv cwb-3.0.0-linux-x86_64.tar.gz
+sed -i -e "s#PREFIX='/usr/local'#PREFIX='/opt/cwb/'#" ./cwb-3.0.0-linux-x86_64/install-cwb.sh
 ./cwb-3.0.0-linux-x86_64/install-cwb.sh
 
 wget http://cwb.sourceforge.net/temp/Perl-CWB-2.2.102.tar.gz
